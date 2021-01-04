3e73b6f7b1b54370228ce15c897ac2b4605223e0
lkwa201 <lkwa201@gmail.com>
2021-01-04 10:34:59 +0900

    * 초 역순 처리 60,59,58...

--- src/DateTime.java
+++ src/DateTime.java
@@ -1,8 +1,6 @@
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Calendar;
 import java.util.Date;
 
 
@@ -12,89 +10,32 @@ public class DateTime {
 	public static void main(String[] args) throws ParseException {
 		DateTime dateTime = new DateTime();
 		dateTime.case2();
-
-
 	}
 
 	public void case2() throws ParseException {
 
 		Date date = new Date();
 		DateFormat stringFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String beginTime = "2020-12-29 16:52:06"; //마지막 제출시각
+		String beginTime = "2021-01-04 10:10:00"; //기준 시간.
 
-		Date endDate = stringFormat.parse(stringFormat.format(date)); //now
+		Date endDate = stringFormat.parse(stringFormat.format(date)); //현재시간(현 날짜+현시간(초 포함)
 		Date beginDate = stringFormat.parse(beginTime);
 		long gap = (endDate.getTime() - beginDate.getTime()) / 1000; // 초 단위
-
 
 		long reminder = ((gap / 60)) % 60; //분
 		long secondGap = gap % 60; //초
 
 		if(reminder < 30) {
 			System.out.println("제출불가");
-			String modTime = String.format("%02d",30-reminder)
-					+ ":" + String.format("%02d", secondGap);
-
-			System.out.println(modTime);
+			String nextTime = String.format("%02d",30-reminder)
+					+ ":" + String.format("%02d", 60-secondGap);
+			System.out.println(nextTime);
 
 		} else {
 			System.out.println("제출가능");
 		}
 
-
-
-
-
-//		Date ldate = new Date(gap);
-//		SimpleDateFormat dateFormat =new SimpleDateFormat("HH:mm:ss");
-//		System.out.println(dateFormat.format(ldate));
-
-
 	}
 
-//	public void case1() throws ParseException {
-//		DateFormat stringFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		String beginTime = "2020-12-29 13:00:00";
-//		Date date = new Date();
-//
-//		// String을 Date로 포맷
-//		Date beginDate = stringFormat.parse(beginTime);
-//		Date endDate = stringFormat.parse(stringFormat.format(date));
-//
-//		long gap = (endDate.getTime() - beginDate.getTime()) / 1000; // 초 단위
-//
-//		long hourGap = gap / 60 / 60;
-//		long reminder = ((gap / 60)) % 60;
-//		long minuteGap = reminder;
-//		long secondGap = gap % 60;
-//
-//		if (hourGap > 99) {
-//			hourGap = 99;
-//		}
-//
-//
-//		String returnTime = String.format("%02d", hourGap)
-//				+ ":" + String.format("%02d", minuteGap)
-//				+ ":" + String.format("%02d", secondGap);
-//
-//
-//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//
-//		Date begin = new Date();
-//		Calendar cal = Calendar.getInstance();
-//		;
-//		cal.add(Calendar.MINUTE, 30);
-//		String today = sdf.format(cal.getTime());
-//		System.out.println(today);
-//
-//
-//		Date end = new Date();
-//
-//		Duration duration = Duration.between(beginDate.toInstant(), begin.toInstant());
-//		System.out.println("남은 분 : " + duration.toMinutes());
-//		System.out.println("남은 초 : " + duration.toMillis());
-//
-//		//System.out.println(returnTime);
-//	}
 
 }
